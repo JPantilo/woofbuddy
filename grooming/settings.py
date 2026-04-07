739e60712da1f7ce2ffab2b170806182c57fbbd7
JPantilo <jayvensanchez.pantilo@my.smciligan.edu.ph>
2026-04-07 17:22:40 +0800

    Update settings.py

--- grooming/settings.py
+++ grooming/settings.py
@@ -37,7 +37,10 @@ ROOT_URLCONF = 'grooming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -48,6 +51,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'grooming.wsgi.application'
 
