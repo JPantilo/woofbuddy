f1c054f776564fa6a4ea24486ec7a9621c3f394d
JPantilo <jayvensanchez.pantilo@my.smciligan.edu.ph>
2026-04-08 14:34:21 +0800

    Design: Fix registration form styling

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email', 'phone_number', 'address')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
